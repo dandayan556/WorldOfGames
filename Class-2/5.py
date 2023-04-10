b7ff358075f1b6a0e79680f5bbdfda2ea8bbd336
Dan Dayan <dandayan556@gmail.com>
2023-04-10 18:24:04 +0300

    another commit

--- Class-2/5.py
+++ Class-2/5.py
@@ -6,7 +6,7 @@ strThree =" Three"
 is_today_a_good_day = a>b and (strOne == "aviel" or b>1 )
 if is_today_good_day:
     print("a is greater than b")
-elif not a==b:
+elif not a==b:# if/else
     print("a equals then b")
 elif strOne != strThree:
     print("blabla")
--- Class-2/fibo-10.py
+++ Class-2/fibo-10.py
@@ -1,5 +1,5 @@
 #import fibo
 from fibo import fib as first_fib
-from danfibo import fib as second_fib
+from danfibo import fib as
 first_fib(100)
 second_fib(100)
