99aeac83a5a9bdc828826c176fe537b7f3abbf75
Dan Dayan <129217354+dandayan556@users.noreply.github.com>
2023-06-30 07:42:50 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -1,35 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 
-def test_scores_service(url: str) -> bool:
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    try:
-        score_element = driver.find_element(By.ID, 'score')
-        score_text = score_element.text
-        score = int(score_text)
-        if 1 <= score <= 1000:
-            return True
-        else:
-            return False
-    except:
-        return False
-    finally:
-        driver.quit()
+def test_scores_service():
+    my_driver = webdriver.Chrome(ChromeDriverManager().install())
+    my_driver.get("http://localhost:8777/")
+    my_score = my_driver.find_element_by_xpath("/html/body/main/div/div[1]/table/tbody/tr/td[2]").text
+    my_int_score = int(my_score)
+    print(my_int_score.__bool__())
+    return 0 < my_int_score < 1000
 
 
 def main_function():
-    url = 'http://127.0.0.1:5000'
-    result = test_scores_service(url)
-
-    if result:
-        return 0
+    """call our tests function. The main function will return -1 as an OS exit
+    code if the tests failed and 0 if they passed."""
+    if test_scores_service:
+        return exit(0)
     else:
-        return -1
+        return exit(-1)
 
 
-if __name__ == '__main__':
-    exit_code = main_function()
-    exit(exit_code)
+test_scores_service()
+main_function()
